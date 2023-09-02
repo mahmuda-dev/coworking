be3beb3395dc5390bda41f89ee732c86d2122530
mahmuda-dev <mahmudaakhter2468@gmail.com>
2023-09-02 12:09:46 +0600

    complete the coworking project

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        dots:true,
+        arrows:false
+      });
+  });
+
+   
